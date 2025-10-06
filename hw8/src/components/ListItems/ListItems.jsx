fd70eebeeb9ed75f536233a70a5da4330a129754
Aleksandra Kotenko <bubochka.a.kotenko@gmail.com>
2025-10-06 14:14:31 +0200

    add new hw8

---
+++ hw8/src/components/ListItems/ListItems.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+
+
+const ListItems = () => {
+  const [items, setItems] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  useEffect(() => {
+    console.log('Компонент ListItems обновлен')
+  }, [items])
+
+  const addItem = () => {
+    if (inputValue.trim() === '') 
+      return
+    setItems([...items, inputValue.trim()])
+    setInputValue('')
+  }
+
+  return (
+    <div>
+      <h2>Список элементов</h2>
+      <input 
+      type="text"
+      placeholder="Введите элемент"
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Добавить</button>
+
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ListItems;
